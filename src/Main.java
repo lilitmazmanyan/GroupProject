88ccebf3666eb2bab0e0f2cc0a274f721d1cf51f
Lilit <lilit.mazmanyan1@gmail.com>
2019-12-07 21:36:46 +0400

    Added GetInput and RecognizeEmployees methods

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public EpamEmployee[] epamEmployees;
     public static String getInput(){
         //Ask the user for the name of employee and keep it in a string
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the full name of epam employee");
+        String name = input.next();
+    return name;
     }
 
     public static void showEmployeeNamesMenu(){
@@ -12,18 +15,28 @@ public class Main {
         //ask the user to enter the name he wants to search
 
     }
+
     public static int recognizeEmployee(){
         //call getInput() and keep the string in a variable
         //search the value in the employees array
         //if there's a match return that employee's index
         //else return -1
+        String name = getInput();
+        for (int i = 0; i < epamEmployees.length; i++){
+            if(epamEmployees[i] == name)
+                return i;
+            else
+                return -1;
+        }
     }
+
     public static void showMenu(){
         //Show what info we can show, enumerating it with (a,b,c)
         //ask the user for a letter
         //add a switch with the letter as argument
         //show output for that input
     }
+
     public static instantiateObjects(){
         //add all employee info
         //store employees in the epamEmployees array
