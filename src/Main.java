cd13c6e89dcbda4b06066ada93d559ca39782764
zakoyangayane <gayanezakoyan@gmail.com>
2019-12-08 15:49:41 +0400

    changes in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,47 @@
 import java.util.Scanner;
 
 public class Main {
-    public EpamEmployee[] epamEmployees;
-    public static String getInput(){
+    public static EpamEmployee[] epamEmployees;
+
+    public static String getInput() {
         //Ask the user for the name of employee and keep it in a string
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the full name of epam employee");
+
         String name = input.next();
-    return name;
+        return name;
     }
 
-    public static void showEmployeeNamesMenu(){
+    public static void showEmployeeNamesMenu() {
+        System.out.println("Here are some epam employees");
         System.out.println("Narek Musakhanyan");
         System.out.println("Edik Mkoyan");
         System.out.println("Hayk Sargsyan");
         System.out.println("Artem Sukiasyan");
         System.out.println("Artur Arutyunyan");
+        System.out.println();
+        System.out.println("Please enter the full name of epam employee");
         getInput();
-        //Show all the names of empam employees
+        //Show all the names of epam employees
         //ask the user to enter the name he wants to search
 
     }
 
-    public static int recognizeEmployee(){
+    public static int recognizeEmployee() {
         //call getInput() and keep the string in a variable
         //search the value in the employees array
         //if there's a match return that employee's index
         //else return -1
         String name = getInput();
-        for (int i = 0; i < epamEmployees.size(); i++){
-            if(epamEmployees.get(i).equals(name)) {
+        for (int i = 0; i < epamEmployees.length; i++) {
+            if (epamEmployees[i].equals(name)) {
                 return i;
             }
         }
         return -1;
     }
 
-    public static void showMenu(){
+    public static void showMenu() {
+        System.out.println("What info would you like to search about him?");
         System.out.println("hometown");
         System.out.println("topSkills");
         System.out.println("favoritePhrase");
@@ -44,32 +49,37 @@ public class Main {
         System.out.println("Lecturing subject");
         System.out.println("workingInEpam");
         System.out.println("Birthday");
-        System.out.println("Please enter your choice");
-        Scanner sc=new Scanner(System.in);
-        String choice = sc.nextLine();
+
+        String choice = getInput();
         switch (choice) {
-            case  ():
+            case ("hometown"): {
                 //;
                 break;
-            case (topSkills):
-               // ;
+            }
+            case ("topSkills"): {
+                // ;
                 break;
-...
-            case (favoritePhrase):
-              //  ;
-                break;
-            case (University):
+            }
+            case ("favoritePhrase"): {
                 //  ;
                 break;
-            case (Lecturing subject):
+            }
+            case ("University"): {
                 //  ;
                 break;
-            case (workingInEpam):
+            }
+            case ("Lecturing subject"): {
                 //  ;
                 break;
-            case (Birthday):
+            }
+            case ("workingInEpam"): {
                 //  ;
                 break;
+            }
+            case ("Birthday"): {
+                //  ;
+                break;
+            }
 
         }
         //Show what info we can show, enumerating it with (a,b,c)
@@ -78,7 +88,7 @@ public class Main {
         //show output for that input
     }
 
-    public static instantiateObjects(){
+    public static instantiateObjects() {
         //add all employee info
         //store employees in the epamEmployees array
         //instantiate EpamArmenia class
@@ -86,8 +96,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        boolean continueTheProgram=true;
+        boolean continueTheProgram = true;
         int index;
         while (continueTheProgram) {
             do {
@@ -99,12 +108,10 @@ public class Main {
                 }
                 int index = recognizeEmployee(nameToSearch);
             } while (index == -1);
-            if(continueTheProgram) {
+            if (continueTheProgram) {
                 EpamEmployee selectedEmployee = epamEmployees[index];
                 showMenu();
             }
         }
-
-
     }
 }
